bc33b7d2c3bed62fdb43bfa12f7e4dbd34da690f
Jason Aravanis <64725836+jasonaravanis@users.noreply.github.com>
2023-12-16 18:14:44 +1100

    feat: add eslint-config-prettier

--- eslint.config.js
+++ eslint.config.js
@@ -2,10 +2,12 @@ import js from "@eslint/js";
 import tsParser from "@typescript-eslint/parser";
 import reactPlugin from "eslint-plugin-react";
 import hooksPlugin from "eslint-plugin-react-hooks";
+import eslintConfigPrettier from "eslint-config-prettier";
 import globals from "globals";
 
 export default [
   js.configs.recommended,
+  eslintConfigPrettier,
   {
     files: ["**/*.ts", "**/*.tsx"],
     languageOptions: {
