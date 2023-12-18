85793562d4c5ed6fc9cad85aa0316a44f5c2de25
Jason Aravanis <64725836+jasonaravanis@users.noreply.github.com>
2023-12-18 18:43:44 +1100

    chore: refactor

--- src/components/tuner/lookup-closest-note/index.ts
+++ src/components/tuner/lookup-closest-note/index.ts
@@ -16,8 +16,8 @@ export const getClosestNote = (frequency: number) => {
     "B",
   ];
 
-  // Based on formula found in https://newt.phys.unsw.edu.au/jw/notes.html
-  const result = Math.round(12 * Math.log2(frequency / A4_FREQUENCY) + 69);
-  const index = result % 12;
+  // Derive MIDI note number, credit to https://newt.phys.unsw.edu.au/jw/notes.html
+  const midi = Math.round(12 * Math.log2(frequency / A4_FREQUENCY)) + 69;
+  const index = midi % 12;
   return notes[index];
 };
