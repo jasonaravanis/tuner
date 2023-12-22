9e4fadb595a980911dc5aec1c7bb45045cf00e4d
Jason Aravanis <64725836+jasonaravanis@users.noreply.github.com>
2023-12-22 20:33:51 +1100

    feat: replace tailwind with vanilla extract

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
-import tailwind from "@astrojs/tailwind";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react()],
+  vite: {
+    plugins: [vanillaExtractPlugin()],
+  },
 });
